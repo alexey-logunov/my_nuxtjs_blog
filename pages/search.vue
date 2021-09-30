c956c18cb0da7ec29bf63efbaba271173e8e72b5
alexey-logunov <al_logunov@mail.ru>
2021-09-30 17:14:58 +0300

    search

--- pages/search.vue
+++ pages/search.vue
@@ -1,13 +1,23 @@
 <template>
-  <h1>Привет, это страница поиска</h1>
+  <div>
+    <SearchHeader @searchPosts="posts = $event"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <nav aria-label="breadcrumb" class="mt-4">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><nuxt-link to="/">Главная</nuxt-link></li>
+              <li class="breadcrumb-item active" aria-current="page">Поиск</li>
+            </ol>
+          </nav>
+          <p class="lead">Найдено записей: {{posts.count}}</p>
+          <div v-for="post in posts.results" :key="post.id">
+            <nuxt-link :to="`/posts/${post.slug}`"><h2>{{ post.h1 }}</h2></nuxt-link>
+            <p v-html="post.description"></p>
+            <hr>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-
-<script>
-export default {
-  name: "search"
-}
-</script>
-
-<style scoped>
-
-</style>
