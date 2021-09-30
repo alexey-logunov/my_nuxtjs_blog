02631c168664c7961e554e0990b3e8ab377dcf83
alexey-logunov <al_logunov@mail.ru>
2021-09-30 20:48:30 +0300

    validate forms

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,8 +32,9 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+plugins: [
+	{ src: '~/plugins/vuelidate' }
+],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
